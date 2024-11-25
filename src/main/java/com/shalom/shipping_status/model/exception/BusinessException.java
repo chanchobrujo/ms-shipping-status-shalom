575302e9d76e8d167ded96ecf94ee4510cc7c5d2
chanchobrujo <85182515+chanchobrujo@users.noreply.github.com>
2024-11-25 16:59:31 -0500

    REFACTOR: Remove cors.

--- src/main/java/com/shalom/shipping_status/model/exception/BusinessException.java
+++ src/main/java/com/shalom/shipping_status/model/exception/BusinessException.java
@@ -1,6 +1,9 @@
 package com.shalom.shipping_status.model.exception;
 
+import java.io.Serial;
+
 public class BusinessException extends RuntimeException {
+    @Serial
     private static final long serialVersionUID = 874387341803198776L;
 
     public BusinessException(String message) {
