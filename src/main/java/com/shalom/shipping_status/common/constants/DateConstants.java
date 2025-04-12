2514c0249bc31b9d518e5585236c416ab66b8a10
chanchobrujo <85182515+chanchobrujo@users.noreply.github.com>
2025-04-12 17:02:34 -0500

    FIX: Deployment.

--- src/main/java/com/shalom/shipping_status/common/constants/DateConstants.java
+++ src/main/java/com/shalom/shipping_status/common/constants/DateConstants.java
@@ -6,7 +6,7 @@ import java.util.Locale;
 
 import static java.time.format.DateTimeFormatter.ofPattern;
 
-public record DateConstants() {
+public record DateConstants() {//
     public static final ZoneId ZONE_ID = ZoneId.of("America/Lima");
     public static final DateTimeFormatter FORMATTER = ofPattern("EEEE, dd MMMM, yyyy ', a las' HH 'con' MM 'minutos'", new Locale("es", "ES"));
 
